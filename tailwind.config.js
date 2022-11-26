83412859c0323b4ee76ec4a43d0144ee0e78ef27
Grivia Trifosa <68889648+Viyaaa@users.noreply.github.com>
2022-11-26 22:22:44 +0700

    Add navbar
    belum ada button dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'light-color': '#9747FF',
+                'dark-color': '#3B2166',
+            }
+        },
     },
     plugins: [],
 }
