26277a38c3d9d4d848f76b93e8cbd092eee22e7c
NicoLe-01 <nicosiahaan9898@gmail.com>
2022-11-26 15:59:21 +0800

    ubah proyek menjadi full react

--- webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const config = require('./webpack.config.js');
-
-module.exports = merge(config, {
-    mode: 'development',
-    devtool: false,
-});
