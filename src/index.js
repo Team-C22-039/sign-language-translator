b86efc1325a6eb4faf5e6e1078cf126ffd605206
Team-C22-039 <m266x0591@dicoding.org>
2022-11-26 19:34:37 +0700

    Update index.js #1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const header = ReactDOM.createRoot(document.getElementById("header"));
-
+/* eslint-disable */
+const header = ReactDOM.createRoot(document.getElementById('header'));
 
 header.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+const footer = ReactDOM.createRoot(document.getElementById('footer'));
