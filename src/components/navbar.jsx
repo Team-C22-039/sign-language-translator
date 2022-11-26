e586384182dcb2b60a850ddc269c7b847ed860ef
NicoLe-01 <nicosiahaan9898@gmail.com>
2022-11-26 20:42:39 +0800

    buat field image dan text input dan file input

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex justify-between p-8 bg-[#9747FF]">
+    <div className="navbar flex justify-between px-8 py-6 bg-[#9747FF]">
       <div className="text-3xl font-[Poppins]">Product Name</div>
       <div>Dark</div>
     </div>
