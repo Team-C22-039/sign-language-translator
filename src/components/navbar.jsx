43f1f0cc58e61d5d673f6f7c46f1969b33a05d7f
NicoLe-01 <nicosiahaan9898@gmail.com>
2022-11-26 16:29:45 +0800

    menambahkan tailwind css

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return <h1 className="text-3xl font-bold underline"> Hello World</h1>;
+};
+
+const Test = () => {
+  return <div className="div"></div>;
+};
+
+export { Navbar, Test };
