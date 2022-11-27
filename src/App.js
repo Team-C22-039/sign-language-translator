1ca0b8100e2669e1b49c3eddb7ba0606c9fce355
unknown <asyqor246thoriq@gmail.com>
2022-11-27 13:47:52 +0700

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from "./components/main";
+import { Navbar } from "./components/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
