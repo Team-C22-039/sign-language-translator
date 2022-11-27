0e86e94a36541518ecdc0b7ffc26852a8f35f142
Team-C22-039 <m266x0591@dicoding.org>
2022-11-27 21:27:24 +0700

    Deactivate config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
-// postcss.config.js
-module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'tailwindcss/nesting': 'postcss-nesting',
-        tailwindcss: { config: './tailwind.config.js' },
-        tailwindcss: {},
-        autoprefixer: {},
-    }
-}
+// // postcss.config.js
+// module.exports = {
+//     plugins: {
+//         'postcss-import': {},
+//         'tailwindcss/nesting': 'postcss-nesting',
+//         tailwindcss: { config: './tailwind.config.js' },
+//         tailwindcss: {},
+//         autoprefixer: {},
+//     }
+// }
