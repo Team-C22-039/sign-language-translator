2a40065544f3233c6e7d62909bc5704da2fad905
Team-C22-039 <m266x0591@dicoding.org>
2022-11-29 05:32:07 +0700

    Adding new component to display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'flowbite';
-import $ from 'jquery';
-import Switch from './components/modeSwtich';
+import './index.css'
+import 'flowbite'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BsGithub } from "react-icons/bs"
+import Switch from './components/modeSwtich'
+import ImageOutput from './components/imageOutput'
+import Creator from './components/creator'
+import Copyright from './components/copyright'
+import MicInput from './components/micBtn'
 
 /* eslint-disable */
 const header = ReactDOM.createRoot(document.getElementById('head'));
@@ -16,4 +20,31 @@ header.render(
     </React.StrictMode>
 )
 
-const footer = ReactDOM.createRoot(document.getElementById('footer'));
+const main = ReactDOM.createRoot(document.getElementById("content"));
+
+main.render(
+    <React.StrictMode>
+        <ImageOutput />
+        <MicInput />
+    </React.StrictMode>
+)
+
+const footer = ReactDOM.createRoot(document.getElementById('foot'));
+
+footer.render(
+    <React.StrictMode>
+        <h2 className='text-2xl md:text-3xl mx-4 my-5 md:my-5 font-Poppins transition-all ease-in-out duration-300'>Creator</h2>
+        <div className='flex text-base md:text-lg mx-4 gap-x-[100px] md:my-[-6px] flex flex-col md:flex-row md:flex-wrap
+        transition-all ease-in-out duration-300'>
+            {/* <h2 className="text-xl md:text-4xl">Creator</h2> */}
+            <Creator creator="Nico Siahaan" />
+            <Creator creator="Asyqor Thoriq" />
+            <Creator creator="Rayhanantha Akbar Putra Prasetyo" />
+            <Creator creator="Grivia Trifosa Iskandar" />
+        </div>
+        <div className='flex mt-4 mx-4 cursor-pointer'>
+            <BsGithub className='mx-2 text-2xl hover:text-[#DFDFDF]' />
+        </div>
+        <Copyright />
+    </React.StrictMode>
+)
