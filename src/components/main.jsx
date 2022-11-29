a8e22fc47e623d84c633f521897044cabfab3d25
NicoLe <nicosiahaan9898@gmail.com>
2022-11-29 22:58:42 +0800

    memperbaiki kode yang error
    ya maap

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function Main() {
   const [isListening, setIsListening] = useState(false);
   const [textResult, setTextResult] = useState("");
   const [imResult, setImageResult] = useState("../images/catto.jpg");
-  
+
   const {
     transcript,
     listening,
@@ -15,38 +16,38 @@ function Main() {
   } = useSpeechRecognition();
 
   useEffect(() => {
-    handleRecord()
-  }, [isListening])
+    handleRecord();
+  }, [isListening]);
 
   const resetInput = () => {
     resetTranscript();
     document.getElementById("inputText").value = "";
-  }
+  };
 
   const handleRecord = () => {
-    if(isListening) {
+    if (isListening) {
       // menghapus string yang ada ataupun yang belum ada sebelum mulai rekam
       resetTranscript();
 
       // start record audio dengan bahasa Indonesia dan terus merekam
-      SpeechRecognition.startListening({language: 'id-ID', continuous: true});
+      SpeechRecognition.startListening({ language: "id-ID", continuous: true });
     } else {
       // stop record audio
       SpeechRecognition.stopListening();
-      
+
       // constraint untuk pertama kali web dirender
-      if(transcript === "") {
+      if (transcript === "") {
         console.log("Not contain word to convert");
       } else {
         document.getElementById("inputText").value = transcript;
       }
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     convertToSign(e.target.sentence.value);
-  }
+  };
 
   const convertToSign = (word) => {
     var textSentence = word;
@@ -60,66 +61,41 @@ function Main() {
           setImageResult(`../images/Alphabet-img/` + letter + `.jpg`);
         }, index * 1000);
       });
-  }
-
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition,
-  } = useSpeechRecognition();
+  };
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
-  function updateInput() {
-    document.getElementById("input-text").value = transcript;
-  }
-
-  function startListen() {
-    SpeechRecognition.startListening({
-      continuous: true,
-      language: "id",
-    });
-  }
-
-  function stopListen() {
-    SpeechRecognition.stopListening();
-    updateInput();
-  }
-
-  function resetInput() {
-    resetTranscript();
-    document.getElementById("input-text").value = "";
-  }
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
-  
   return (
     <div className="bg-[#E4E4E4]">
       <div className="flex lg:flex-row md:flex-col flex-col justify-around">
         <div className="flex flex-col items-center container-img border-2 border-black m-28 lg:p-20">
-            <img src={imResult} className="h-56 w-56 aspect-square" alt="Image Result" id="imageResult" />
-            <h1>{textResult}</h1>
+          <img
+            src={imResult}
+            className="h-56 w-56 aspect-square"
+            alt="img-result"
+            id="imageResult"
+          />
+          <h1>{textResult}</h1>
         </div>
         <div className="flex-col lg:m-28 md:m-0">
           <div className="input_container">
             <div className="flex justify-center">
               <div className="mb-3 xl:w-96">
                 <div id="microphone">
-                  <p className="text-center lg:text-start">Microphone : {listening ? "on" : "off"}</p>
+                  <p className="text-center lg:text-start">
+                    Microphone : {listening ? "on" : "off"}
+                  </p>
                   <div className="flex flex-row space-x-2 justify-center lg:justify-start">
                     <button
                       type="button-start"
-                      className={ listening ? 
-                        "inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase  rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out" 
-                        : 
-                        "inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                      className={
+                        listening
+                          ? "inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase  rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+                          : "inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                       }
-                      onClick={() => setIsListening(prevState => !prevState)}
+                      onClick={() => setIsListening((prevState) => !prevState)}
                     >
                       {listening ? "Stop" : "Start"}
                     </button>
@@ -162,11 +138,14 @@ function Main() {
                     id="inputText"
                     rows="3"
                     name="sentence"
-                    placeholder="Masukkan Text"/>
-                    <button
-                      className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-3 mx-32" type="submit">
-                      Submit
-                    </button>
+                    placeholder="Masukkan Text"
+                  />
+                  <button
+                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-3 mx-32"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
                 </form>
               </div>
             </div>
@@ -176,5 +155,4 @@ function Main() {
     </div>
   );
 }
-<></>;
 export default Main;
