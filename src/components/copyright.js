82f6e0cf23a6b19e56410f28fae4ad5a2b7d4985
Team-C22-039 <m266x0591@dicoding.org>
2022-12-05 12:30:17 +0700

    Changing class utility

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Copyright() {
     return (
-        <div className='flex justify-center my-2 mt-7 md:my-2 md:mt-10 ml-4 md:ml-0 transition-all ease-in-out duration-300'>
-            <span className=''>&copy;2022 Product Name. All Rights Reserved</span>
+        <div className='md:text-center mt-[2rem] my-1 md:mt-10 ml-4 md:ml-0 transition-all ease-in-out duration-300'>
+            <span className="text-white">&copy;2022 Product Name. All Rights Reserved</span>
         </div>
     )
 }
