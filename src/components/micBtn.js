19ab2595e3afc77fbc9e5bb6c480e316d0dd2985
Team-C22-039 <m266x0591@dicoding.org>
2022-12-05 12:31:48 +0700

    Changing mic button class

--- src/components/micBtn.js
+++ src/components/micBtn.js
@@ -5,10 +5,10 @@ export default function MicInput(props) {
     const [micOn, setMicOn] = React.useState(false)
 
     return (
-        <button id="mic-input" className={`p-3 bg-slate-100 h-min w-min border-[1px] border-black rounded-xl
+        <button id="mic-input" className={`p-2 md:p-3 bg-slate-100 h-min w-min border-[1px] border-black rounded-lg md:rounded-xl
         hover:bg-slate-300 active:bg-slate-500 cursor-pointer transition-all ease-in duration-100 ${props.className}`}
             onClick={() => { micOn === false ? setMicOn(true) : setMicOn(false) }}>
-            <BsFillMicFill className="text-2xl text-black" />
+            <BsFillMicFill className="text-lg md:text-2xl text-black" />
         </button>
     )
 }
