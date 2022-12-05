5f93181e895114b04fe5f0909ed6e40044890f7e
Team-C22-039 <m266x0591@dicoding.org>
2022-12-06 05:31:14 +0700

    Re-positioning

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Copyright() {
     return (
-        <div className='md:text-center mt-[2rem] my-1 md:mt-10 ml-4 md:ml-0 transition-all ease-in-out duration-300'>
-            <span className="text-white">&copy;2022 Product Name. All Rights Reserved</span>
+        <div className='md:text-center mt-[2rem] ml-4 md:ml-0 transition-all ease-in-out duration-300'>
+            <span className="text-white">&copy;2022 Team C22-039. All Rights Reserved</span>
         </div>
     )
 }
