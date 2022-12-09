0353d2556a67222ded290d2eb1322ecb3e41b26d
Team-C22-039 <m266x0591@dicoding.org>
2022-12-09 20:29:03 +0700

    Adding Text-to-Speech feature

--- src/components/inputBox.js
+++ src/components/inputBox.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import $ from 'jquery'
 import { BsFillVolumeUpFill } from "react-icons/bs"
 import MicInput from "./micBtn"
@@ -7,12 +7,24 @@ import MicInput from "./micBtn"
 export default function InputBox(props) {
     const content = document.getElementById('content');
     $(content).addClass('container mx-auto my-10')
+
+    const [myText, setMyText] = useState("")
+    const msg = new SpeechSynthesisUtterance(myText)
+
+    const speechHandler = (msg) => {
+        msg.lang = 'id-ID'
+        window.speechSynthesis.speak(msg)
+        // window.speechSynthesis.speak(lang).lang('id-ID')
+    }
+
     return (
         <div id="input-box" className="drop-shadow-[0_0_7px_rgba(0,0,0,0.2)] sm:mx-auto sm:w-[90vw] md:w-[80vw] md:h-[50vh] xl:w-[30vw]">
             <div className="flex md:flex-row">
                 <textarea id="text-input" className="p-5 h-[40vh] sm:h-[20vh] md:h-[42vh] font-Poppins text-lg sm:text-xl md:text-2xl xl:text-xl rounded-t-lg bg-slate-100 resize-none 
             border-none focus:ring-2 focus:ring-black flex-grow text-black
-            scrollbar-thin scrollbar-track-[#CFD5DD] scrollbar-thumb-black scrollbar-thumb-rounded-2xl scrollbar-thumb-cursor-pointer" placeholder="Masukkan teks disini.."></textarea>
+            scrollbar-thin scrollbar-track-[#CFD5DD] scrollbar-thumb-black scrollbar-thumb-rounded-2xl scrollbar-thumb-cursor-pointer" placeholder="Masukkan teks disini.."
+                    onChange={(e) => setMyText(e.target.value)}>
+                </textarea>
             </div>
             <div id="sound-view" className="bg-slate-100 gap-x-4 p-4 rounded-b-xl
             flex items-center justify-end border-t-2 border-t-slate-300 flex-grow-0
@@ -20,7 +32,7 @@ export default function InputBox(props) {
                 {/* <FileInput /> */}
                 <div>
                     <BsFillVolumeUpFill className="text-[30px] md:text-4xl text-black cursor-pointer hover:text-slate-700
-                    transition-all ease-in duration-200" onClick={() => { }} />
+                    transition-all ease-in duration-200" onClick={() => speechHandler(msg)} />
                 </div>
                 <MicInput />
             </div>
