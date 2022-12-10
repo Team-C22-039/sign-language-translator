113f9c8f92891af9cf36c831c9c603453bfbfe64
Team-C22-039 <m266x0591@dicoding.org>
2022-12-10 16:08:21 +0700

    Delete copyright component

--- src/components/copyright.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Copyright() {
-    return (
-        <div className='md:text-center mt-[2rem] ml-4 md:ml-0 transition-all ease-in-out duration-300'>
-            <span className="text-white">&copy;2022 Team C22-039. All Rights Reserved</span>
-        </div>
-    )
-}
