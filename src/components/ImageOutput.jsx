4056746bb3b3bb34b67ebb14e88240de91aa2b13
Team-C22-039 <m266x0591@dicoding.org>
2022-12-11 15:07:55 +0700

    Update component

--- src/components/ImageOutput.jsx
+++ src/components/ImageOutput.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-export default function ImageOutput(props) {
+function ImageOutput(props) {
 	return (
-		<div className="w-full mx-auto sm:mx-0">
+		<div className="w-full md:w-fit lg:w-full mx-auto sm:mx-0">
 			<div
 				id="image-box"
-				className="flex flex-col gap-y-5 mx-5 p-10 drop-shadow-[2px_2px_10px_rgba(0,0,0,0.6)] bg-slate-600 rounded-lg transition-all ease-out duration-300"
+				className="flex flex-col md:flex-row xl:flex-col items-center xl:items-start justify-center
+				gap-y-5 gap-x-5 mx-5 p-10 drop-shadow-[2px_2px_10px_rgba(0,0,0,0.6)] 
+				bg-slate-600 rounded-lg transition-all ease-out duration-300"
 			>
-				<div id="img-output" className="w-[200px]">
+				<div className="">
 					<img
-						src={require(`./images/Dummy.png`)}
+						id="img-output"
+						src={props.imgResult}
 						alt=""
-						className="p-5 bg-black"
+						className="w-[50vw] md:w-[40vw] lg:w-[25vw] xl:w-[16vw]"
 					/>
-				</div>
-				<div>
-					<input type="file" name="" id="img-input" />
-				</div>
-				<div id="img-preview" className="">
-					<img src={require(`./images/Dummy.png`)} alt="" className="" />
+					<h3 className="text-xl text-center bg-white">{props.textInput}</h3>
 				</div>
 			</div>
 		</div>
 	);
 }
+
+export { ImageOutput };
